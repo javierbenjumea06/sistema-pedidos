ec0a086eef02efa955d095dbde334228e9758a51
Tomas Vega <tomas.vega.9012@gmail.com>
2025-09-25 18:55:34 -0500

    Creamos un main

---
+++ main.js
@@ -0,0 +1,38 @@
+const menu= [
+    {        
+      id:"00A30",
+      nombre:"Hamburguesa Clasica",
+      Precio:"20000",
+      categoria: "Hamburguesas" 
+    },
+    {        
+      id:"00A31",
+      nombre:"Hamburguesa Doble Queso",
+      Precio:"22000",
+      categoria: "Hamburguesas" 
+    },
+    {        
+      id:"00A32",
+      nombre:"Hamburguesa Pollo Crispy",
+      Precio:"25000",
+      categoria: "Hamburguesas" 
+    },
+    {        
+      id:"00B30",
+      nombre:"Perro Clasico",
+      Precio:"19000",
+      categoria: "Perros" 
+    },
+    {        
+      id:"00B31",
+      nombre:"Perro Doble Queso",
+      Precio:"20000",
+      categoria: "Perros" 
+    },
+    {        
+      id:"00B32",
+      nombre:"Perro crispy",
+      Precio:"22000",
+      categoria: "Perros" 
+    },
+]; 
