ea7ac0999d14969e88247a7eb2c959fe3e448357
Tomas Vega <tomas.vega.9012@gmail.com>
2025-09-25 18:46:25 -0500

    listando los productos :)

--- Menu.js
+++ Menu.js
@@ -42,3 +42,14 @@ const Product = { id: Idproducto, nombre, Precio };
 products.push(product);
 console.log(`Producto agregado: #${product.id} - ${product.name} ($${product.price}`);
 }
+//  listar productos ---
+function listaproductos() {
+  if (products.length === 0) {
+    console.log("No hay productos en el menú.");
+    return;
+  }
+  console.log("=== Menú de productos ===");
+  products.forEach(p => {
+    console.log(`${p.id} - ${p.name} - ${p.price}`)
+  });
+}
