b0bbeb1bc311e8cf5502f27850c60593baef61ae
Tomas Vega <tomas.vega.9012@gmail.com>
2025-09-25 18:30:37 -0500

    Elementos

--- Menu.js
+++ Menu.js
@@ -1,8 +1,20 @@
 const menu= [
     {        
-      id:"00A32",
+      id:"00A30",
       nombre:"Hamburguesa Clasica",
       Precio:"20000",
-      categoria: "HAmburguesas" 
+      categoria: "Hamburguesas" 
     },
-];
+    {        
+      id:"00A31",
+      nombre:"Hamburguesa Doble Queso",
+      Precio:"22000",
+      categoria: "Hamburguesas" 
+    },
+    {        
+      id:"00A32",
+      nombre:"Hamburguesa Pollo Crispy",
+      Precio:"25000",
+      categoria: "Hamburguesas" 
+    },
+]; // Aquí guardaremos los productos
