e96c6382eff4e7af2aa3b74f662eeb8ad9338341
Tomas Vega <tomas.vega.9012@gmail.com>
2025-09-25 20:04:09 -0500

    correcion de detallitos

--- Menu.js
+++ Menu.js
@@ -1,6 +1,45 @@
+const menu= [
+    {        
+      id:"00A30",
+      nombre:"Hamburguesa Clasica",
+      Precio:"20000",
+      categoria: "Hamburguesas" 
+    },
+    {        
+      id:"00A31",
+      nombre:"Hamburguesa Doble Queso",
+      Precio:"22000",
+      categoria: "Hamburguesas" 
+    },
+    {        
+      id:"00A32",
+      nombre:"Hamburguesa Pollo Crispy",
+      Precio:"25000",
+      categoria: "Hamburguesas" 
+    },
+    {        
+      id:"00B30",
+      nombre:"Perro Clasico",
+      Precio:"19000",
+      categoria: "Perros" 
+    },
+    {        
+      id:"00B31",
+      nombre:"Perro Doble Queso",
+      Precio:"20000",
+      categoria: "Perros" 
+    },
+    {        
+      id:"00B32",
+      nombre:"Perro crispy",
+      Precio:"22000",
+      categoria: "Perros" 
+    },
+]; 
+
 function agregarproductos(menu) {
 const product = { 
-  id: "00A78",
+  id: "00B33",
   nombre: "Perro Peperoni", 
   Precio: "25000",
   categoria: "Perros" };
@@ -24,8 +63,8 @@ const productoBuscado1 = "Hamburguesa"
 const productoBuscado2 = "Pizza"
 
 function buscarproducto(productoBuscado) {
-  const resultados = products.filter(p => p.nombre.includes(productoBuscado));
-  if (results.length === 0) {
+  const resultados = menu.filter(p => p.nombre.includes(productoBuscado));
+  if (resultados.length === 0) {
     console.log(`No se encontraron productos con: "${productoBuscado}"`)
   } else {
     console.log(`Resultados de búsqueda para "${productoBuscado}"`)
