c069439d542f3c3777c26ddf2686ecbf6b5b66b4
unknown <admin@infopc1.bga.upb>
2025-09-25 20:28:57 -0500

    Función crear pedido terminada en un 100%

--- pedidos.js
+++ pedidos.js
@@ -1,35 +1,18 @@
-/*
-2. Gestión de pedidos
- Crear un pedido con: idPedido, nombreCliente, items[] (lista de Productos), total(precios), estado (pendiente, preparando, entregado).
- Cambiar el estado de un pedido.
- Filtrar pedidos por estado.	
-*/
-
-
 function crearPedido(pedidos) {
     var pedido1 = {idPedido: "4", nombreCliente: "Esteban", items: ["Hamburguesa Clasica", "Hamburguesa Pollo Crispy"]}
     pedidos.push(pedido1)
     console.log("pedido añadido")
 }
 function cambiarEstado(pedidos) {
-    
+    pedidos[0].estado = "preparando"
 }
-/*
-  let total = items.reduce((sum, item) => sum + item.precio, 0);
 
+let pedidos = [{
 
-  return {
-    idPedido,
-    nombreCliente,
-    items,
-    total,
-    estado: ESTADOS[0] 
-  };
+    idPedido: "1", nombreCliente: "Novelia", items: ["Hamburguesa Clasica", "Perro Clasico"], estado : "pendiente"},
+    {idPedido: "2", nombreCliente: "Kyu", items: ["Perro Doble Queso", "Perro crispy"], estado: "pendiente"},
+    {idPedido: "3", nombreCliente: "Rubén", items: ["Perro Doble Queso", "Perro Clasico"], estado: "pendiente"},
+]
 
-*/
-
-let pedidos = {
-    idPedido: "1", nombreCliente: "Novelia", items: ["Hamburguesa Clasica", "Perro Clasico"],
-    idPedido: "2", nombreCliente: "Kyu", items: ["Perro Doble Queso", "Perro crispy"],
-    idPedido: "3", nombreCliente: "Rubén", items: ["Perro Doble Queso", "Perro Clasico"],
-};
+crearPedido(pedidos)
+cambiarEstado(pedidos)
