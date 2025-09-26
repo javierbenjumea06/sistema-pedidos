26aecf6dc5fd84e20326a75aff93c0cf73966644
unknown <admin@BGAI201-005.bga.upb>
2025-09-25 19:25:40 -0500

    menu de funciones completo

--- reportes.js
+++ reportes.js
@@ -35,18 +35,51 @@ const menu = console.log(`ACCEDE A UNO DE LOS MENUS:
         3. Reportes.
         4. Salir.`)
 
- if (menu == 1) {
-        const menu1 = console.log(`ELIGE UNA DE LAS FUNCIONES: 
-        1. Agregar un producto.
-        2. Listar los productos.
-        3. Buscar productos por nombre o categoria.`)
-    } else if (menu == 2) {
-        const menu2 = console.log(`ELIGE UNA DE LAS FUNCIONES: 
-        1. Crear un pedido.
-        2. Cambiar el estado de un pedido.
-        3. Filtrar pedidos por estado.`)
-    } else if (menu == 3) {
-        const menu3 = console.log(`ELIGE UNA DE LAS FUNCIONES: 
-        1. Mostrar cuantos pedidos hay por estado.
-        2. Calcular el total de ventas`)
+if (menu == 1) {
+    const menu1 = console.log(`ELIGE UNA DE LAS FUNCIONES: 
+    1. Agregar un producto.
+    2. Listar los productos.
+    3. Buscar productos por nombre o categoria.`)
+
+    if (menu1 == 1){
+        agregarproductos(nombre, precio)
+    }  else if (menu1 == 2){
+        listaproductos(menu)
+    } else if (menu1 == 3){
+        buscarproducto(menu)
+    } else {
+        console.log("Escribiste algo invalido")
     }
+} else if (menu == 2) {
+    const menu2 = console.log(`ELIGE UNA DE LAS FUNCIONES: 
+    1. Crear un pedido.
+    2. Cambiar el estado de un pedido.
+    3. Filtrar pedidos por estado.`)
+
+    if (menu2 == 1){
+        crearPedido()
+    }  else if (menu2 == 2){
+        cambiarEstado()
+    } else if (menu2 == 3){
+        filtrar()
+    } else {
+        console.log("Escribiste algo invalido")
+    }
+
+} else if (menu == 3) {
+    const menu3 = console.log(`ELIGE UNA DE LAS FUNCIONES: 
+    1. Mostrar cuantos pedidos hay por estado.
+    2. Calcular el total de ventas`)
+
+    if (menu3 == 1){
+        pedidosPorEstado()
+    }  else if (menu2 == 2){
+        totalVentas()
+    } else {
+        console.log("Escribiste algo invalido")
+    }
+
+} else {
+    console.log("Decidiste salir")
+}
+
