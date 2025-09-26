95532833cf8482ea2f1cfb4c229ee238a476a51d
Tomas Vega <tomas.vega.9012@gmail.com>
2025-09-25 19:15:59 -0500

    pasamos el menu al main

--- Menu.js
+++ Menu.js
@@ -1,55 +1,30 @@
-const menu= [
-    {        
-      id:"00A30",
-      nombre:"Hamburguesa Clasica",
-      Precio:"20000",
-      categoria: "Hamburguesas" 
-    },
-    {        
-      id:"00A31",
-      nombre:"Hamburguesa Doble Queso",
-      Precio:"22000",
-      categoria: "Hamburguesas" 
-    },
-    {        
-      id:"00A32",
-      nombre:"Hamburguesa Pollo Crispy",
-      Precio:"25000",
-      categoria: "Hamburguesas" 
-    },
-    {        
-      id:"00B30",
-      nombre:"Perro Clasico",
-      Precio:"19000",
-      categoria: "Perros" 
-    },
-    {        
-      id:"00B31",
-      nombre:"Perro Doble Queso",
-      Precio:"20000",
-      categoria: "Perros" 
-    },
-    {        
-      id:"00B32",
-      nombre:"Perro crispy",
-      Precio:"22000",
-      categoria: "Perros" 
-    },
-]; // Aquí guardaremos los productos
-let Idproducto = 1; // Para dar un ID único a cada producto
-function agregarproductos(nombre,Precio){
-const Product = { id: Idproducto, nombre, Precio };
+
+let Idproducto = 1; 
+function agregarproductos(nombre,precio){
+const Product = { id: Idproducto, nombre, precio };
 products.push(product);
 console.log(`Producto agregado: #${product.id} - ${product.name} ($${product.price}`);
 }
-//  listar productos ---
+
 function listaproductos() {
   if (products.length === 0) {
     console.log("No hay productos en el menú.");
     return;
   }
-  console.log("=== Menú de productos ===");
+  console.log(" Menú de productos");
   products.forEach(p => {
     console.log(`${p.id} - ${p.name} - ${p.price}`)
   });
 }
+function buscarproduc(query) {
+  const results = products.filter(p => p.name.toLowerCase().includes(query.toLowerCase()));
+  if (results.length === 0) {
+    console.log(`No se encontraron productos con: "${query}"`)
+  } else {
+    console.log(`Resultados de búsqueda para "${query}"`)
+    results.forEach(p => {
+      console.log(`#${p.id} - ${p.name} - $${p.price}`)
+    });
+  }
+}
+
